f44e87aa8d16d9ce1d0edbce2c7e2be61c2e4133
alanh2 <alanhelouani@gmail.com>
2020-02-10 20:47:55 -0300

    Diseño + terminacion de la pantalla 3 con popup

--- Avi/TFSWebOffline3/Js/alan.js
+++ Avi/TFSWebOffline3/Js/alan.js
@@ -1,7 +1,5 @@
 $(document).ready(function () {
-    console.log(navigator.userAgent.indexOf('Mac'));
     if(navigator.userAgent.indexOf('Mac') > 0){
-        console.log("entro en mac");
         $('body').addClass('mac-os');
     }
 });
