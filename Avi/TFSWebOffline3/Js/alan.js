ba24ed70055c652940ee956485f56eba22b49942
alanh2 <alanhelouani@gmail.com>
2020-02-11 02:01:13 -0300

    diseño y detalles faltantes de las 3 pamtallas

--- Avi/TFSWebOffline3/Js/alan.js
+++ Avi/TFSWebOffline3/Js/alan.js
@@ -2,4 +2,23 @@ $(document).ready(function () {
     if(navigator.userAgent.indexOf('Mac') > 0){
         $('body').addClass('mac-os');
     }
+
 });
+    function OnMenuClick(Name) {
+        inactiveMenus();
+        activeMenu(Name);
+    }
+
+    function inactiveMenus() {
+        $('#menu1 > a').removeClass('active');
+        $('#menu2 > a').removeClass('active');
+        $('#menu3 > a').removeClass('active');
+        $('#menu4 > a').removeClass('active');
+        $('#menu5 > a').removeClass('active');
+        $('#LiGroups > a').removeClass('active');
+        $('#LiRules > a').removeClass('active');
+    }
+
+    function activeMenu(Name) {
+        $('#'+Name+' > a').addClass('active');
+    }
